48fdd21ce00056437d88400e833d152175cf0412
Jack Lenox <jack.lenox@gmail.com>
2015-01-16 09:31:27 +0000

    Remove unnecessary chunks of debug code from function.php

--- functions.php
+++ functions.php
@@ -10,12 +10,9 @@ function picard_scripts() {
 add_action( 'wp_enqueue_scripts', 'picard_scripts' );
 
 function get_json( $_post ) {
-	$counter;
 	foreach ( $_post as $post ) {
 		$_post['post_class'] = implode( ' ', get_post_class( $_post['id'] ) );
-		$counter++;
 	}
-	// error_log( print_r( $_post, true ) );
 	return $_post;
 }
 
@@ -32,7 +29,6 @@ add_action( 'wp_json_server_before_serve', 'picard_api_init' );
 class Picard_API_Comments {
 	public function register_routes( $routes ) {
 		$routes['/picard/comments'] = array(
-			array( array( $this, 'get_posts' ), WP_JSON_Server::READABLE ),
 			array( array( $this, 'new_post' ), WP_JSON_Server::CREATABLE ),
 		);
 
@@ -40,7 +36,6 @@ class Picard_API_Comments {
 	}
 
 	public function new_post() {
-		// error_log( print_r( $_POST, true ) );
 		$commentdata = array(
 			'comment_post_ID'      => $_POST['comment_post_ID'],
 			'comment_author'       => $_POST['comment_author'],
@@ -49,12 +44,7 @@ class Picard_API_Comments {
 			'comment_content'      => $_POST['content'],
 			'comment_author_IP'    => $_SERVER['REMOTE_ADDR'],
 		);
-		// error_log( print_r( $commentdata, true ) );
 		$comment_id = wp_new_comment( $commentdata );
 		error_log( $comment_id );
-	}
-
-	public function get_posts() {
-		error_log( "aaaaaaah" );
 	}
 }
