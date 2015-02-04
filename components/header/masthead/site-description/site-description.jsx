03c7d34da5ab83e919dcde296c98953abe57aa14
kwight <kwight@kwight.ca>
2015-02-03 17:55:34 -1000

    Add site title and site description components.

---
+++ components/header/masthead/site-description/site-description.jsx
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ */
+var React = require( 'react' );
+
+/**
+ * Internal dependencies
+ */
+
+
+/**
+ * 
+ */
+module.exports = React.createClass( {
+	componentWillMount: function() {},
+	componentDidMount: function() {},
+	componentWillUnmount: function() {},
+
+	render: function() {
+		return (
+			<h2 className="site-description">site description</h2>
+		);
+	}
+} );
---
+++ components/header/masthead/site-title/site-title.jsx
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ */
+var React = require( 'react' );
+
+/**
+ * Internal dependencies
+ */
+
+
+/**
+ * 
+ */
+module.exports = React.createClass( {
+	componentWillMount: function() {},
+	componentDidMount: function() {},
+	componentWillUnmount: function() {},
+
+	render: function() {
+		return (
+			<h1 className="site-title">site title</h1>
+		);
+	}
+} );
