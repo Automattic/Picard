3b21c865206016a0e213b4ddfdb02c3358170737
michaeldcain <michael@michaeldcain.com>
2015-02-04 10:43:12 -1000

    Remove the stuff that Jack lazily left in the head.

--- index.php
+++ index.php
@@ -7,11 +7,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-	<?php wp_head(); /* ?>
-	<script type="text/javascript" src="<?php echo get_template_directory_uri() . '/libs/react.min.js'; ?>"></script>
-	<script type="text/javascript" src="<?php echo get_template_directory_uri() . '/libs/JSXTransformer.js'; ?>"></script>
-	<script type="text/jsx" src="<?php echo get_template_directory_uri() . '/js/picard.js'; ?>"></script>
-	*/ ?>
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
