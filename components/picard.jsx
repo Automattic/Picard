6e63aa66063d00bae1e76a312471883e0b01223b
Jack Lenox <jack.lenox@gmail.com>
2015-02-05 11:34:16 -1000

    Add new router component

--- components/picard.jsx
+++ components/picard.jsx
@@ -7,7 +7,7 @@ var React = require( 'react/addons' ),
 /**
  * Internal dependencies
  */
-var Content = require( './content/content.jsx' );
+var Router = require( './router/router.jsx' );
 
 /**
  * A quick and dirty way of hijacking any clicks on the navigation and passing the requests to our router
@@ -20,46 +20,53 @@ jQuery( 'li.page_item a' ).click( function( e ) {
 });
 
 /**
+ * Make it so…
+ */
+React.render(
+	<Router />, document.getElementById( 'main' )
+);
+
+/**
  * The routing of what is effectively the site index
  * @todo Move routing into its own component
  * @todo Don't fire .render when route changes
  * @todo Write more comprehensive routing based on permalink structure of blog
  */
-page( '/', function() {
-	React.render(
-		<Content url="/wp-json/posts" />,
-		document.getElementById( 'main' )
-	);
-});
+// page( '/', function() {
+// 	React.render(
+// 		<Content url="/wp-json/posts" />,
+// 		document.getElementById( 'main' )
+// 	);
+// });
 
 /**
  * Post link route
  * @todo As above
  */
-page( '/:year/:month/:day/:slug', function(ctx, next) {
-	var slug = ctx.params.slug;
-	url = "/wp-json/posts/?filter[name]=" + slug;
-	React.render(
-		<Content url={url} />,
-		document.getElementById( 'main' )
-	);
-});
+// page( '/:year/:month/:day/:slug', function(ctx, next) {
+	// var slug = ctx.params.slug;
+	// url = "/wp-json/posts/?filter[name]=" + slug;
+	// React.render(
+	// 	<Content url={url} />,
+	// 	document.getElementById( 'main' )
+	// );
+// });
 
 /**
  * Other link route
  * @todo As above
  */
-page( '*', function(ctx, next ) {
-	var slug = ctx.pathname;
-	if(slug.substr(-1) == '/') {
-		slug = slug.substr(0, slug.length - 1);
-	}
-	var part = slug.substring(slug.lastIndexOf('/') + 1);
-	url = "/wp-json/posts/?type[]=page&filter[name]=" + part;
-	React.render(
-		<Content url={url} />,
-		document.getElementById( 'main' )
-	);
-});
+// page( '*', function(ctx, next ) {
+// 	var slug = ctx.pathname;
+// 	if(slug.substr(-1) == '/') {
+// 		slug = slug.substr(0, slug.length - 1);
+// 	}
+// 	var part = slug.substring(slug.lastIndexOf('/') + 1);
+// 	url = "/wp-json/posts/?type[]=page&filter[name]=" + part;
+// 	React.render(
+// 		<Content url={url} />,
+// 		document.getElementById( 'main' )
+// 	);
+// });
 
-page();
+// page();
---
+++ components/router/router.jsx
@@ -0,0 +1,52 @@
+/**
+ * External dependencies
+ */
+var React = require( 'react/addons' ),
+	page = require( 'page' );
+
+/**
+ * Internal dependencies
+ */
+var Content = require( '../content/content.jsx' );
+
+var Router = React.createClass({
+
+	componentDidMount: function() {
+		
+		var self = this;
+
+		page('/', function ( ctx ) {
+			self.setState({ component: <Content url="/wp-json/posts" /> });
+		});
+
+		page('/:year/:month/:day/:slug', function ( ctx ) {
+			var slug = ctx.params.slug;
+			url = "/wp-json/posts/?filter[name]=" + slug;
+			self.setState({ component: <Content url={url} /> });
+		});
+
+		page('*', function ( ctx ) {
+			var slug = ctx.pathname;
+			if(slug.substr(-1) == '/') {
+				slug = slug.substr(0, slug.length - 1);
+			}
+			var part = slug.substring(slug.lastIndexOf('/') + 1);
+			var url = "/wp-json/posts/?type[]=page&filter[name]=" + part;
+			self.setState({ component: <Content url={url} /> });
+		});
+
+		page.start();
+
+	},
+
+	getInitialState: function() {
+		return { component: <div /> };
+	},
+
+	render: function() {
+		return this.state.component;
+	}
+
+});
+
+module.exports = Router;
