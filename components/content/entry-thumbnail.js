c41212b1f96be34627d90c49574b56cff47cd691
Thomas Guillot <thomas@automattic.com>
2015-02-06 18:11:26 -1000

    Fix entry-thumbnail height

--- components/content/entry-thumbnail.js
+++ components/content/entry-thumbnail.js
@@ -7,45 +7,52 @@ var entry_thumbnail = ( function( $ ) {
 
 	function single_entry_thumbnail() {
 
+		var site_header_height        = $( '.site-header' ).outerHeight(),
+		    entry_thumbnail           = $( '.entry-thumbnail' ),
+		    entry_thumbnail_img       = entry_thumbnail.find( 'img' );
+		if ( $( 'body' ).hasClass( 'admin-bar' ) ) {
+			var admin_bar_height      = $( '#wpadminbar' ).outerHeight();
+			site_header_height       += admin_bar_height;
+		}
+
+		entry_thumbnail_img.each( function() {
+			var img = $( this ),
+			    new_img = new Image();
+			new_img.src = img.attr( 'src' );
+			img.removeAttr( 'width' ).removeAttr( 'height' );
+			$( new_img ).load( function() {
+				var img_width = new_img.width,
+				    img_height = new_img.height,
+				    ratio_screen = entry_thumbnail.width() / entry_thumbnail.height(),
+				    ratio_image = img_width / img_height;
+				if ( ratio_image >= ratio_screen ) {
+					img.css( {
+						'height': site_header_height,
+						'max-width': 'none'
+					} );
+				} else {
+					img.css( {
+						'height': '',
+						'max-width': '100%'
+					} );
+				}
+			} );
+		} );
+
+		$( '.entry-header' ).each( function() {
+			$( this ).css( 'margin-top', - $( this ).outerHeight() / 2 );
+		} );
+
 		if ( ( $( 'body' ).hasClass( 'page' ) || $( 'body' ).hasClass( 'single' ) ) && $( '.hentry' ).hasClass( 'has-post-thumbnail' ) ) {
 
-
-			var site_header_height        = $( '.site-header' ).outerHeight(),
-			    entry_thumbnail           = $( '.entry-thumbnail' ),
-			    entry_thumbnail_img       = entry_thumbnail.find( 'img' ),
-			    entry_header              = $( '.entry-header' );
-			if ( $( 'body' ).hasClass( 'admin-bar' ) ) {
-				var admin_bar_height      = $( '#wpadminbar' ).outerHeight();
-				site_header_height       += admin_bar_height;
-			}
-			entry_thumbnail_img.removeAttr( 'width' ).removeAttr( 'height' )
-			                   .css( 'min-height', $( window ).height() - site_header_height );
 			entry_thumbnail.height( $( window ).height() - site_header_height );
+			entry_thumbnail_img.css( 'min-height', $( window ).height() - site_header_height );
 
-			entry_thumbnail_img.each( function() {
-				var img = $( this ),
-				    new_img = new Image();
-				new_img.src = img.attr( 'src' );
-				$( new_img ).load( function() {
-					var img_width = new_img.width,
-					    img_height = new_img.height,
-					    ratio_screen = entry_thumbnail.width() / entry_thumbnail.height(),
-					    ratio_image = img_width / img_height;
-					if ( ratio_image >= ratio_screen ) {
-						img.css( {
-							'height': site_header_height,
-							'max-width': 'none'
-						} );
-					} else {
-						img.css( {
-							'height': '',
-							'max-width': '100%'
-						} );
-					}
-				} );
+		} else {
+
+			entry_thumbnail.each( function() {
+				$( this ).height( $( this ).find( '.entry-header' ).outerHeight() );
 			} );
-			
-			entry_header.css( 'margin-top', - entry_header.outerHeight() / 2 );
 
 		}
 
