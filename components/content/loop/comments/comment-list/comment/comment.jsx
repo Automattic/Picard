b0ade18c5e77a5ca9cb349f074464b561e26eca2
kwight <kwight@kwight.ca>
2015-02-06 15:58:13 -1000

    Fill out comment author and content info based on a single comment prop, passed from CommentList.

--- components/content/loop/comments/comment-list/comment/comment.jsx
+++ components/content/loop/comments/comment-list/comment/comment.jsx
@@ -9,14 +9,24 @@ var React = require( 'react/addons' );
 var Comment = React.createClass({
 
 	render: function() {
+		var comment = this.props.comment,
+			author = this.props.comment.author;
+
 		return (
-			<li id="comment-{this.props.id}">
-				<article id="div-comment-{this.props.id}" className="comment-body">
+			<li id={"comment-" + comment.ID}>
+				<article id={"div-comment-" + comment.ID} className="comment-body">
 					<footer className="comment-meta">
 						<div className="comment-author vcard">
+							<img src={author.avatar} className="avatar avatar-56" height="56" width="56" />
+							<b className="fn">
+								<a href={author.URL} rel="external nofollow" className="url">{author.name}</a>
+							</b>
+						</div>
+						<div className="comment-metadata">
+							<time datetime={comment.date}>{comment.date}</time>
 						</div>
 					</footer>
-					<div className="comment-content" dangerouslySetInnerHTML={{__html: this.props.content}} />
+					<div className="comment-content" dangerouslySetInnerHTML={{__html: comment.content}} />
 					<div className="reply">
 						<a className="comment-reply-link">Reply</a>
 					</div>
